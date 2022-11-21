11a03aca4fc08c6c7c67a03c6b2555adf16cba60
Max Morgan <maxmmorgan93@gmail.com>
2022-11-21 10:23:08 -0600

    init commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,28 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //console write line prompting user for info
+
+            Console.WriteLine("Enter your Name");
+
+            //declaring a variable name to capture user input with Readline
+
+            string name = Console.ReadLine();
+
+            //prompt user to enter age
+           
+            Console.WriteLine("Input your age");
+
+            // record user answer onto age variable
+            int age = int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine($"Hi {name}, you are {age} years old.");
+            
+
+
         }
     }
 }
